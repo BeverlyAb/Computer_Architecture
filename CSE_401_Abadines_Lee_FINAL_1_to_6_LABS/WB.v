a95c79348e8eae2c6bbda7ac5674e200ddba93eb
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2016-09-25 23:20:10 -0700

    Update WB.v

--- CSE_401_Abadines_Lee_FINAL_1_to_6_LABS/WB.v
+++ CSE_401_Abadines_Lee_FINAL_1_to_6_LABS/WB.v
@@ -13,11 +13,11 @@ The inputs are mem_Read_data, mem_ALU_result, and MemtoReg which are the outputs
 module WB(
 	input 	wire [31:0]	mem_Read_data,
 	input	wire [31:0]	mem_ALU_result,
-	input 	wire 			MemtoReg,
-	output	wire	[31:0]wb_data
+	input 	wire 		MemtoReg,
+	output	wire [31:0]	wb_data
     );
 	 
-	mux mux_5(.y(wb_data), // output
+	mux mux_5		(.y(wb_data), // output
 				.a(mem_Read_data),
 				.b(mem_ALU_result),
 				.sel(MemtoReg));
