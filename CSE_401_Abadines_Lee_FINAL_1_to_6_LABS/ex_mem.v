6cba93956b0642b7f862b907329092e34bb09f5a
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2016-09-25 23:29:15 -0700

    Update ex_mem.v

--- CSE_401_Abadines_Lee_FINAL_1_to_6_LABS/ex_mem.v
+++ CSE_401_Abadines_Lee_FINAL_1_to_6_LABS/ex_mem.v
@@ -10,18 +10,18 @@ Date: May 18th, 2016
 This is the latch that receives signals form all the modules of the execute stage. Its outputs go to MEM/WB and FETCH.
 */
 module ex_mem(
-	input		wire	[1:0]		ctlwb_out,
-	input		wire	[2:0]		ctlm_out,
-	input		wire	[31:0]	adder_out,
-	input		wire				aluzero,
-	input		wire	[31:0]	aluout, readdat2,
-	input		wire	[4:0]		muxout,
-	output	reg	[1:0]		wb_ctlout,
-	output	reg				branch, memread, memwrite, //output of M control line from ID/EX latch (lab 2-2)
+	input	wire	[1:0]	ctlwb_out,
+	input	wire	[2:0]	ctlm_out,
+	input	wire	[31:0]	adder_out,
+	input	wire		aluzero,
+	input	wire	[31:0]	aluout, readdat2,
+	input	wire	[4:0]	muxout,
+	output	reg	[1:0]	wb_ctlout,
+	output	reg		branch, memread, memwrite, //output of M control line from ID/EX latch (lab 2-2)
 	output	reg	[31:0]	add_result,
-	output	reg				zero,
+	output	reg		zero,
 	output	reg	[31:0]	alu_result, rdata2out,
-	output	reg	[4:0]		five_bit_muxout
+	output	reg	[4:0]	five_bit_muxout
     );
 
 	initial begin
